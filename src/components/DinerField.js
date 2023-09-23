5a23ad13c6ab27cfc4446385ed862e10f16873b3
arq.cristianmasci@gmail.com <arq.cristianmasci@gmail.com>
2023-09-23 19:21:37 +1000

    5

--- src/components/DinerField.js
+++ src/components/DinerField.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import '../style/DinerField.css'
 import { useCalculateMoney } from '../hooks/useCalculateMoney'
 import pigFace from '../assets/pig.png'
